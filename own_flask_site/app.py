05813985be45ac4c84a10baae7632c9ebd62e5ef
arturmaievskyi <113231491+arturmaievskyi@users.noreply.github.com>
2023-08-25 10:52:28 +0200

    Add files via upload

--- own_flask_site/app.py
+++ own_flask_site/app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from seetings import SECRET_KEY
 from sql_queries import*
 
+#________________________________________________________________________________________#
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = SECRET_KEY 
 
+#________________________________________________________________________________________#
 
 db_pizzas = ShopDB_Pizzas()
 db_drinks = ShopDB_Drinks()
 
+#________________________________________________________________________________________#
 
 @app.route("/templates/index.html")
 @app.route("/")
@@ -46,12 +49,50 @@ def drinkPage():
     print(drink_obj)
     return render_template('drink.html',title="Online Pizzeria",  drinks = drink_obj)
 
+@app.route("/templates/pizza/<pizza_id>")
+def order_pizza(pizza_id):
+    pizza_obj = db_pizzas.get_pizza(pizza_id)
+    if request.method == 'POST':
+        try:
+            db_pizzas.add_order_pizza(pizza[0], 
+                    request.form["name"],
+                    request.form["phone"],
+                    request.form["email"],
+                    request.form["city"],
+                    request.form["address"], 
+                        pizza[5])
+            flash("Додано замовлення!", "alert-light") #надсилаємо швидкі сповіщення у браузер
+            return redirect(url_for('index')) #перенаправляємо на головну сторінку
+        except:
+            flash("Помилка оформлення замовлення!", "alert-danger") #надсилаємо швидкі сповіщення у браузер
+    return render_template('new_order_pizza.html', title="Online Pizzeria", pizza = pizza_obj)
+
+@app.route("/templates/drink/<drink_id>")
+def order_drink(drink_id):
+    drink_obj = db_drinks.get_drink(drink_id)
+    if request.method == 'POST':
+        try:
+            db_drinks.add_order_drink(drink[0], 
+                    request.form["name"],
+                    request.form["phone"],
+                    request.form["email"],
+                    request.form["city"],
+                    request.form["address"], 
+                        drink[5])
+            flash("Додано замовлення!", "alert-light") #надсилаємо швидкі сповіщення у браузер
+            return redirect(url_for('index')) #перенаправляємо на головну сторінку
+        except:
+            flash("Помилка оформлення замовлення!", "alert-danger") #надсилаємо швидкі сповіщення у браузер
+    return render_template('new_order_drink.html', title="Online Pizzeria", drink = drink_obj)
+
 
 @app.route("/templates/about.html")
 def About():
     return render_template("about.html", title="Online Pizzeria")
 
 
+
+#________________________________________________________________________________________#
 if __name__ == "__main__":
     app.run(debug=True)
     app.config['TEMPLATES_AUTO_RELOAD'] = True
--- own_flask_site/sql_queries.py
+++ own_flask_site/sql_queries.py
@@ -27,6 +27,28 @@ class ShopDB_Pizzas:
         data_one_pizza = self.cursor.fetchone()
         self.close_pizzas()
         return data_one_pizza
+    
+    def add_order_pizza(self, *data):
+        self.open_pizzas()
+        self.cursor.execute('''INSERT INTO orders (item_id, name, phone, email, city, address, cost)
+                            VALUES((?), (?), (?), (?), (?), (?), (?))''', [*data])
+        self.conn.commit()
+        self.close_pizzas()
+
+    def get_categories(self):
+        self.open_pizzas()
+        self.cursor.execute("SELECT * FROM categories")
+        data = self.cursor.fetchall()
+        self.close()
+        return data
+    
+
+    def get_category_pizza(self, id):
+        self.open_pizzas()
+        self.cursor.execute("SELECT * FROM items WHERE category_id==(?)", [id])
+        data = self.cursor.fetchall()
+        self.close_pizzas()
+        return data
 class ShopDB_Drinks:
     def __init__(self):
         self.conn = None
@@ -36,6 +58,7 @@ class ShopDB_Drinks:
     def open_drinks(self): #функція що відкриває базу даних
         self.conn =sqlite3.connect(self.db_name) # з'єднання з базою даних (БД)
         self.cursor = self.conn.cursor() #курсор (посилання) на БД
+
 
     def close_drinks(self): #функція що закриває базу даних
         self.cursor.close()
@@ -53,4 +76,25 @@ class ShopDB_Drinks:
         data_one_drink = self.cursor.fetchone()
         self.close_drinks()
         return data_one_drink
+    def add_order_drink(self, *data):
+        self.open_drinks()
+        self.cursor.execute('''INSERT INTO orders (item_id, name, phone, email, city, address, cost)
+                            VALUES((?), (?), (?), (?), (?), (?), (?))''', [*data])
+        self.conn.commit()
+        self.close()
+
+    def get_categories_drink(self):
+        self.open_drinks()
+        self.cursor.execute("SELECT * FROM categories")
+        data = self.cursor.fetchall()
+        self.close()
+        return data
+    
+
+    def get_category_drinks(self, id):
+        self.open_drinks()
+        self.cursor.execute("SELECT * FROM items WHERE category_id==(?)", [id])
+        data = self.cursor.fetchall()
+        self.close()
+        return data
     
